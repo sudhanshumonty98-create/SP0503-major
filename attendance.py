ce24757fd8381feb1e2a3174464ccebfd9cf677e
sudhanshumonty98-create <sudhanshu.monty98@gmail.com>
2026-02-13 22:16:05 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -21,7 +21,8 @@ def findEncodings(images):
     encodeList = []
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
+        encode = face_recognition.face_encodings(img,model="hog")[0]# trying hog
+instead of cnn        
         encodeList.append(encode)
     return encodeList
 
@@ -42,14 +43,21 @@ def markAttendance(name):
 
 # cam
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,480)#LOWER RESOLUTION
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,15)
 
+frame_count = 0
+SKIP_FRAMES = 5
+
+frame_count = 0
 while True:
     success, img = cap.read()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+    facesCurFrame = face_recognition.face_locations(imgS,model="hog")
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame,model="hog")
 
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
@@ -61,12 +69,12 @@ while True:
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
             cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
-            cv2.putText(img, name, (x1,y2+25),
+            cv2.putText(img, name, (x1,y2+35),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
             markAttendance(name)
 
     cv2.imshow('Face Attendance System', img)
-    if cv2.waitKey(1) == 13:  #  Enter to exit
+    if cv2.waitKey(1)&0xFF == 13:  #  Enter to exit
        break
 
 cap.release()
